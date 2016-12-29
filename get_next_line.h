2732379b16fd834e861b30adfb95be5936ee1bf5
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2016-12-29 14:23:15 +0100

    macro s_line s_tmp et s_last_cr
    1 moulitest qui plante avec BUFF_SIZE = 1024

--- get_next_line.h
+++ get_next_line.h
@@ -18,18 +18,25 @@
 # include <unistd.h>
 # include <sys/stat.h>
 # include <sys/types.h>
+# include "libft/libft.h"
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1024
 # define GNL_OK 1
 # define GNL_EOF 0
 # define GNL_PB -1
+# define s_line (content_line->line)
+# define s_last_cr (content_line->last_cr)
+# define s_tmp (content_line->tmp)
 
 typedef struct		s_content
 {
 	int				fd;
 	int				pos_last_cr;
+	char			*last_cr;
 	char			*line;
+	char			*tmp;
 }					t_content;
-	
-int		get_next_line(const int fd, char **line);
+
+int					get_next_line(const int fd, char **line);
+
 #endif
